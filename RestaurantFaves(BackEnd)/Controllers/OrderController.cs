e2436fa0181dd039eefb10e65673d0a121246214
DavidBrameijer <davidbrameijer@gmail.com>
2024-10-02 08:06:12 -0400

    First Commit

---
+++ RestaurantFaves(BackEnd)/Controllers/OrderController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantFaves_BackEnd_.Models;
+
+namespace RestaurantFaves_BackEnd_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+         private RestFaveDbContext dbContext = new RestFaveDbContext();
+
+        [HttpGet()]
+        public IActionResult GetAll(string? restaurant = null, bool? orderAgain = null)
+        {
+            List<Order> result = dbContext.Orders.ToList();
+            if (restaurant != null)
+            {
+                result = result.Where(o => o.Restaurant.ToLower().Contains(restaurant.ToLower())).ToList();
+            }
+            if (orderAgain != null)
+            {
+                result = result.Where(o => o.OrderAgain == orderAgain).ToList();
+            }
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            Order result = dbContext.Orders.FirstOrDefault(o => o.Id == id);
+            if (result == null)
+            {
+                return NotFound("No matching id");
+            }
+            else
+            {
+                return Ok(result);
+            }
+        }
+
+        [HttpPost()]
+        public IActionResult AddOrder([FromBody] Order newOrder)
+        {
+            newOrder.Id = 0;
+            dbContext.Orders.Add(newOrder);
+            dbContext.SaveChanges();
+            return Created($"/api/Orders/{newOrder.Id}", newOrder);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateOrder(int id, [FromBody] Order updated)
+        {
+            if(updated.Id != id)
+            {
+                return BadRequest("Ids don't match");
+            }
+            if(!dbContext.Orders.Any(o => o.Id == id))
+            {
+                return NotFound("No matcing ids");
+            }
+            dbContext.Orders.Update(updated);
+            dbContext.SaveChanges();
+            return Ok(updated);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteOrder(int id)
+        {
+            Order result = dbContext.Orders.FirstOrDefault(o => o.Id == id);
+            if (result == null)
+            {
+                return NotFound("No matching id");
+            }
+            else
+            {
+                dbContext.Orders.Remove(result);
+                dbContext.SaveChanges();
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ RestaurantFaves(BackEnd)/Models/RestFaveDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantFaves_BackEnd_.Models;
+
+public partial class RestFaveDbContext : DbContext
+{
+    public RestFaveDbContext()
+    {
+    }
+
+    public RestFaveDbContext(DbContextOptions<RestFaveDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=RestFaveDB; Integrated Security=SSPI;Encrypt=false;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Order__3213E83F09C422B7");
+
+            entity.ToTable("Order");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Description)
+                .HasMaxLength(255)
+                .HasColumnName("description");
+            entity.Property(e => e.OrderAgain).HasColumnName("orderAgain");
+            entity.Property(e => e.Rating).HasColumnName("rating");
+            entity.Property(e => e.Restaurant)
+                .HasMaxLength(255)
+                .HasColumnName("restaurant");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
